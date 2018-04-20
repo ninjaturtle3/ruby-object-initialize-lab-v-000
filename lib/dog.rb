ccd4a66cae6ed908db7af6508787ff74acc6560a
Lluvia Frias <lluvi.frias@gmail.com>
2018-04-20 20:54:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,30 +1,6 @@
 class Dog
-  def initialize(name, breed ="Mutt")
-    @name = dogs_name
-    @breed = breed
-  end
 
-  def name=(dogs_name)
-    @name = dogs_name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
+def initialize(name, breed ="Mutt")
+  @name = name
+  @breed = breed
 end
-
-  fido = Dog.new("Fido", "Pug")
-  fido.name
-  fido.breed
-
-  fido = Dog.new("Fido")
-  fido.name
-  fido.breed
