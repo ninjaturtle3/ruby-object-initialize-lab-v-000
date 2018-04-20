f34dbea58feddd574b6beb1e0ee1bf16ed7a70bb
Lluvia Frias <lluvi.frias@gmail.com>
2018-04-20 20:57:35 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  def initialize(name, breed ="Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+end
+
+
+fido = Dog.new("Fido", "Pug")
+fido.name
+fido.breed
+
+fido = Dog.new("Fido")
+fido.name
+fido.breed
